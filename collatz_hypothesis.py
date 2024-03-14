43249ee8bb8c4f19706813a539b56913e76d9ad0
Sadhana Sudireddy <152226660+sadhacde@users.noreply.github.com>
2024-03-14 14:31:56 -0500

    Add files via upload

---
+++ collatz_hypothesis.py
@@ -0,0 +1,12 @@
+﻿c0 = int(input("Enter a number: "))
+steps = 0
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+        print(c0)
+    elif c0 % 2 == 1:
+        c0 = 3 * c0 + 1
+        print(c0)
+    steps += 1
+
+print("steps =", steps) 
