4f986b637a819ed45eafc0d6ac31c3e74787ec2f
Sadhana Sudireddy <152226660+sadhacde@users.noreply.github.com>
2024-03-17 22:30:12 -0500

    Add files via upload

---
+++ input_list_duplicates.py
@@ -0,0 +1,13 @@
+﻿#collects user inputted list and removes duplicates
+
+my_list = []
+num = int(input("How many items do you want in the list: "))
+
+for n in range(num):
+    item = int(input("Enter a number: "))
+    if item not in my_list:
+        my_list.append(item)
+
+print("The list with unique elements only:")
+print(my_list)
+
