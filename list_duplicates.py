67d769dcf25f713fbb2fbb8219d6bdded46059b0
Sadhana Sudireddy <152226660+sadhacde@users.noreply.github.com>
2024-03-17 22:23:37 -0500

    Add files via upload

---
+++ list_duplicates.py
@@ -0,0 +1,11 @@
+﻿#removes duplicate numbers from list
+
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+my_real_list = []
+
+for l in my_list:
+    if l not in my_real_list:
+        my_real_list.append(l)
+
+print("The list with unique elements only:")
+print(my_real_list)
