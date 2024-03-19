f1a8f08234ff27cdd875b7f696b57e8b11b4a821
Sadhana Sudireddy <152226660+sadhacde@users.noreply.github.com>
2024-03-18 21:36:39 -0500

    Add files via upload

---
+++ 2Darray_chessboard.py
@@ -0,0 +1,53 @@
+﻿EMPTY = "-"
+ROOK = "ROOK"
+KNIGHT = "KNIGHT"
+BISHOP = "BISHOP"
+QUEEN = "QUEEN"
+KING = "KING"
+PAWN = "PAWN"
+
+board = []
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+board[0][1] = KNIGHT
+board[0][6] = KNIGHT
+board[7][1] = KNIGHT
+board[7][6] = KNIGHT
+
+board[0][2] = BISHOP
+board[0][5] = BISHOP
+board[7][2] = BISHOP
+board[7][5] = BISHOP
+
+board[0][3] = QUEEN 
+board[0][4] = KING
+
+board[7][3] = QUEEN
+board[7][4] = KING
+
+board[1][0] = PAWN
+board[1][1] = PAWN
+board[1][2] = PAWN
+board[1][3] = PAWN
+board[1][4] = PAWN
+board[1][5] = PAWN
+board[1][6] = PAWN
+board[1][7] = PAWN
+
+board[6][0] = PAWN
+board[6][1] = PAWN
+board[6][2] = PAWN
+board[6][3] = PAWN
+board[6][4] = PAWN
+board[6][5] = PAWN
+board[6][6] = PAWN
+board[6][7] = PAWN
+
+print(board)
