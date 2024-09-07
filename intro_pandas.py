d537c9816a6c14f4742e81b593c7a67f9cdab1e6
sadhana s <152226660+sadhacde@users.noreply.github.com>
2024-09-06 23:29:14 -0500

    Update intro_pandas.py

--- intro_pandas.py
+++ intro_pandas.py
@@ -1,6 +1,4 @@
-Python 3.11.4 (v3.11.4:d2340ef257, Jun  6 2023, 19:15:51) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-"""
+# uses csv files to read data and organizes them into a dictionary and vice versa
 with open("weather_data.csv") as weather_data:
     weather_list = weather_data.readlines()
     print(weather_list)
